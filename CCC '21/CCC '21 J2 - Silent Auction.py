6fb2e4d1849d3135d73655ae3e26a308a92ab085
= <65200215+Ri-Hong@users.noreply.github.com>
2021-02-27 12:44:16 -0500

    Added problem

--- CCC '21/CCC '21 J2 - Silent Auction.py
+++ CCC '21/CCC '21 J2 - Silent Auction.py
@@ -6,23 +6,27 @@ Problem: https://dmoj.ca/problem/ccc21j2
 
 #Explanation
 '''
-We can use a dictionary to store each person and their bid value. (person name as the key, their bid value as the value)
+We can use a list to store the list of bidder's names and another list to store the bidder's corresponding bid value. A bidder will have their bid value at the same index as their name.
 Then we can use the max function to get the highest bid value. 
-Then search the dictionary to find the first person who has that highest bid value.
+Then search the bid values list to get the index of the highest bid.
+Then use that index to find the person who bid that amount from the bidder's name list.
 '''
 
 #Code
 numBidders = int(input()) #get the number of bidders as an integer
-bidders = {} #stores the bidder's name as the key and bid value as the value. 
+bidders = [] #stores the bidder's names
+bidValues = [] #stores the bidder's bid value
+
 for i in range(numBidders): #loop through each bidder
   personName = input() #get the person's name as input
   bidValue = int(input()) #get the person's bid value as an integer
-  bidders[personName] = bidValue #create a new entry in the dictionary with the person's name as the key and their bid value as the value
+  bidders.append(personName) #add the person's name to the bidders list
+  bidValues.append(bidValue) #add the person's bid value to the bidValues list
 
-highestBid = max(bidders.values()) #get the highest bid value
+highestBid = max(bidValues) #get the highest bid value
 
-#search for the person with the highest bid value. Once that person is found, the loop with exit so that only the first person with that bid amount is printed
-for person in bidders.keys():
-  if bidders[person] == highestBid:
-    print(person)
-    break
+for i in range(numBidders): #loop through the number of bidders by index
+  if bidValues[i] == highestBid: #if the index of the highest bid is found,
+    print(bidders[i]) #print the person at that index
+    break #exit the loop so that if there are people with the same bid value, only the first person gets printed
+
