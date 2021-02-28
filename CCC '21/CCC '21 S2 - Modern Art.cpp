0f2c8b360638a68d9b2135dfbbedb5688d18e6f1
= <65200215+Ri-Hong@users.noreply.github.com>
2021-02-28 10:04:25 -0500

    Added problem

--- CCC '21/CCC '21 S2 - Modern Art.cpp
+++ CCC '21/CCC '21 S2 - Modern Art.cpp
@@ -6,14 +6,15 @@ Problem: https://dmoj.ca/problem/ccc21s2
 
 //Explanation
 /*
-Instead of storing all the 'cells' in a 5000000 by 5000000 2D array, We can just store how many times each row/column is painted using 2 x 5000000 arrays.
-Then we loop through those two arrays and mod each value by 2 since painting a single row/col once is equivalent to painting it three times, 5 times, 7 times, etc.
+Instead of storing all the 'cells' in a 5000000 by 5000000 2D array, We can just store how many times each row/column is painted using 2 x 5000000 boolean arrays.
+True/1 will mean that the row/col has been painted and False/0 will mean that the row/col has NOT been painted. Using booleans to store the state of the row/column
+works since painting a single row/col once is equivalent to painting it three times, 5 times, 7 times, etc.
+Along the way, we also keep a running total of how many rows and columns are painted.
 
 We can observe that a square will olny become golden if either: the row is painted and the column isn't OR the row is not painted and the column is. (If row and 
 column are painted or row and column are both not painted, then the square will remain black) This is similar to the exclusive or operator. 
 
-So we can go through the 2 arrays and count how many rows are painted and how many columns are painted. We can also get the number of unpainted rows/columns. 
-Now to find the number of squares, we can do rowsUnpainted * columnsPainted + rowsPainted * columnsUnpainted.
+Thus, the formula for the number of gold squares is: rowsUnpainted * columnsPainted + rowsPainted * columnsUnpainted.
 */
 
 #include <iostream>
@@ -26,8 +27,8 @@ int rcIndex; //stores the index corresponding to the choice
 int numGoldSquares = 0; //stores the number of gold squares
 int nRowTrues, nColTrues = 0; //stores how many rows and cols are painted 
 int nRowFalses, nColFalses; //stores how many rows and cols are NOT painted 
-array<int, 5000000> rowState = {0}; //stores the number of times each row has been painted
-array<int, 5000000> colState = {0}; //stores the number of times each column has been painted
+array<bool, 5000000> rowState = {0}; //stores the number of times each row has been painted
+array<bool, 5000000> colState = {0}; //stores the number of times each column has been painted
 
 int main() {
   //get input
@@ -37,26 +38,20 @@ int main() {
 
   //loop through all instructions and update the rowState and colState arrays
   for(int i = 0; i < nChoices; i++){
-    scanf("%s", &instruction);
-    scanf("%d", &rcIndex);
+    scanf("%s", &instruction); //'R' or 'C'
+    scanf("%d", &rcIndex); //the col/row number to be flipped
+    rcIndex--; //make the index zero based
 
     if(instruction == 'R'){
-      rowState[rcIndex-1]++;
+      rowState[rcIndex] = !rowState[rcIndex]; //"flip" the row. If it is currently a 1, make it 0 and vice versa
+      nRowTrues += rowState[rcIndex]*2 - 1; //if the new state is true/gold, then we add one to the counter, if it's false/black, we subtract one from the counter
     }
     else if(instruction == 'C'){
-      colState[rcIndex-1]++;
+      colState[rcIndex] = !colState[rcIndex]; //"flip" the row. If it is currently a 1, make it 0 and vice versa
+      nColTrues += colState[rcIndex]*2 - 1; //if the new state is true/gold, then we add one to the counter, if it's false/black, we subtract one from the counter
     }
   }
 
-  //loop through the rowState and colState arrays and mod each value by 2 to turn it into a boolean array. Update nRowTrues and nColTrues along the way
-  for(int i = 0; i < 5000000; i++){
-    if(rowState[i] % 2 == 1){
-      nRowTrues++;
-    }
-    if(colState[i] % 2 == 1){
-      nColTrues++;
-    }
-  }
   //calculate nRowFalses and nRowTrues
   nRowFalses = height - nRowTrues;  
   nColFalses = width - nColTrues;
